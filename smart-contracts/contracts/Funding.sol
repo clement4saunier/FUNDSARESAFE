429ed80e62b3d3b934566b1734167b8b1574bea7
clement <clement.saunier@epitech.eu>
2022-05-07 11:20:35 +0200

    funding contract

---
+++ smart-contracts/contracts/Funding.sol
@@ -0,0 +1,86 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract Funding {
+    using Counters for Counters.Counter;
+    Counters.Counter private _id;
+    address private _owner;
+
+    struct Project {
+        string metadata;
+        bool ongoing;
+        IERC20 token;
+        uint256 goal;
+        uint256 fund;
+    }
+
+    // Mapping from projectId to project
+    mapping(uint256 => Project) public project;
+    // Mapping from projectId to builder
+    mapping(uint256 => address) public builder;
+    // Mapping from projectId to founder to funds
+    mapping(uint256 => mapping(address => uint256)) founder;
+
+    constructor() {
+        _owner = msg.sender;
+    }
+
+    function createFunding(
+        IERC20 token,
+        uint256 goal,
+        string memory metadata
+    ) public {
+        project[_id.current()] = Project(metadata, true, token, goal, 0);
+        builder[_id.current()] = msg.sender;
+    }
+
+    function fund(uint256 projectId, uint256 amount) public {
+        require(
+            project[projectId].token.allowance(msg.sender, address(this)) >=
+                amount,
+            "Fund: Not enough allowance"
+        );
+        require(
+            project[projectId].token.transferFrom(
+                msg.sender,
+                address(this),
+                amount
+            ),
+            "Fund: Transfer failed"
+        );
+
+        founder[projectId][msg.sender] += amount;
+        project[projectId].fund += amount;
+    }
+
+    function withdrawFunds(uint256 projectId) public {
+        require(msg.sender == builder[projectId], "Restricted to builder");
+        require(
+            project[projectId].fund >= project[projectId].goal,
+            "Funding not complete"
+        );
+        require(
+            project[projectId].token.transfer(builder[projectId], project[projectId].fund),
+            "Transfer failed"
+        );
+
+        project[projectId].ongoing = false;
+    }
+
+    function withdrawDonation(uint256 projectId) public {
+        require(founder[projectId][msg.sender] != 0, "No donations for project");
+        require(
+            project[projectId].goal >= project[projectId].fund,
+            "Funding complete"
+        );
+        require(!project[projectId].ongoing, "Funding not complete");
+        require(
+            project[projectId].token.transfer(msg.sender, founder[projectId][msg.sender]),
+            "Transfer failed"
+        );
+    }
+}
--- smart-contracts/test/sample-test.js
+++ smart-contracts/test/sample-test.js
@@ -1,19 +1,70 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+describe("Funding", function() {
+  let funding;
+  let token = [];
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+  beforeEach(async function() {
+    const fundingContract = await ethers.getContractFactory("Funding");
+    const tokenContract = await ethers.getContractFactory("Token");
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+    funding = await fundingContract.deploy();
+    for (let i = 0; i < 5; i++) {
+      token[i] = await tokenContract.deploy();
+      await token[i].deployed();
+    }
+    await funding.deployed();
+  });
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+  it("Should create funding", async function() {
+    const [owner] = await ethers.getSigners();
+    const goal = 100;
+    const metadata = "link";
+    const tkn = token[0];
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+    await funding.createFunding(tkn.address, goal, metadata);
+
+    expect((await funding.project(0)).token).to.equal(tkn.address);
+    expect((await funding.project(0)).goal).to.equal(goal);
+    expect((await funding.project(0)).ongoing).to.equal(true);
+    expect(await funding.builder(0)).to.equal(owner.address);
+  });
+
+  it("Should fund with tokens", async function() {
+    const [owner, giver] = await ethers.getSigners();
+    const goal = 100;
+    const metadata = "link";
+    const tkn = token[0];
+    const donation = 10;
+
+    await funding.createFunding(tkn.address, goal, metadata);
+    await tkn.transfer(giver.address, donation);
+
+    await tkn.connect(giver).approve(funding.address, donation);
+    await funding.connect(giver).fund(0, donation);
+
+    expect(await tkn.balanceOf(giver.address)).to.equal(0);
+    expect(await tkn.balanceOf(funding.address)).to.equal(donation);
+  });
+
+  it("Should withdraw if completed", async function() {
+    const [owner, giver] = await ethers.getSigners();
+    const goal = 100;
+    const metadata = "link";
+    const tkn = token[0];
+    const donation = goal + 50;
+
+    await funding.createFunding(tkn.address, goal, metadata);
+    await tkn.transfer(giver.address, donation);
+
+    await tkn.connect(giver).approve(funding.address, donation);
+    await funding.connect(giver).fund(0, donation / 2);
+    expect(funding.withdrawFunds(0)).to.be.revertedWith("Funding not complete");
+    await funding.connect(giver).fund(0, donation / 2);
+
+    expect((await funding.project(0)).fund).to.equal(donation);
+    await funding.withdrawFunds(0);
+    expect(await tkn.balanceOf(funding.address)).to.equal(0);
   });
 });
