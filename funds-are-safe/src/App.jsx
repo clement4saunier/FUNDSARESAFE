4a23855e0d75b02b4c8d827c108833c6ac3828e2
clement <clement.saunier@epitech.eu>
2022-05-07 11:34:05 +0200

    missing files of last commit

--- funds-are-safe/src/App.jsx
+++ funds-are-safe/src/App.jsx
@@ -7,9 +7,22 @@ import Funding from "./components/pages/Funding";
 import Details from "./components/pages/Details";
 import useWallet from "./components/hooks/useWallet";
 import { WalletContext } from "./components/context/Wallet";
+import Contract from "./components/pages/Contract";
+import { Contract as EthContract } from "ethers";
+import { contract } from "./contract/contract";
+import { utils } from "ethers";
 
 function App() {
-  const wallet= useWallet();
+  const wallet = useWallet();
+  const { abi, address } = contract;
+  const fundingContract =
+    wallet.chainId &&
+    new EthContract(
+      address,
+      new utils.Interface(abi.abi),
+      wallet.readOnly ? wallet.provider : wallet.provider.getSigner()
+    );
+  console.log("contract", fundingContract);
 
   return (
     <div className="App">
@@ -21,6 +34,7 @@ function App() {
             <Route path="/create" element={<Create />} />
             <Route path="/funding" element={<Funding />} />
             <Route path="/funding/:contract" element={<Details />} />
+            <Route path="/contract" element={<Contract />} />
           </Routes>
         </BrowserRouter>
       </WalletContext.Provider>
---
+++ funds-are-safe/src/components/pages/Contract.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Page from "../layout/Page";
+
+export default function Contract() {
+    return (
+        <Page>
+            <h1>OUR SMART CONTRACT</h1>
+            <br/>
+            Form to create a crowfunding
+        </Page>
+    )
+}
