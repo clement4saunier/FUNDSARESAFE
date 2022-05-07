18e01bc126ad7fa7d90013e2d013ba77f82f25bd
clement <clement.saunier@epitech.eu>
2022-05-07 17:20:19 +0200

    more form styling

--- funds-are-safe/src/components/pages/Create.jsx
+++ funds-are-safe/src/components/pages/Create.jsx
@@ -22,34 +22,9 @@ export default function Create() {
                 Enter the following details
                 <br />
                 <ProjectInfo/>
-                <input className={styles.input} placeholder="Project name"/>
-                <input className={styles.input} placeholder="Goal"/>
-                <input className={styles.input} placeholder="Goal"/>
-                <br />
-                <form >
-                    <FormField type="text" name="Name" width="162%" title=" Project name :" display="inline-block" /><br />
-                    <FormField type="text" name="Name" width="162%" title=" Description :" display="inline-block" /><br />
-                    <FormField type="number" name="Name" width="100%" title="Goal :" display="inline-block" />
-                    <FormField type="text" name="Name" width="100%" title="token :" display="inline-block" marginLeft="2%" /> <br />
-                    <input type="file" style={{ marginBottom: "1.5%" }} />
-
-                    <Select options={options}
-                        theme={(theme) => {
-                            console.log(theme.colors) 
-                            return ({
-                                ...theme,
-                                borderRadius: 6,
-                                colors: {
-                                    ...theme.colors,
-                                    primary25: "#4ec1f7",
-                                    primary: "#008770",
-                                    neutral0:"#16161c"
-                                }
-                            })
-                        }} />
-
-                    <br /><button type="submit">Create my Project</button>
-                </form>
+                <h2>TELL THE PEOPLE</h2>
+                <input className={styles.markdown} placeholder="The details of your project, why it'll be worht it (full markdown)"/>
+                <button>Mint your crowdfunding!</button>
             </Page>
         )
     } else {
--- funds-are-safe/src/components/pages/create/ProjectInfo.jsx
+++ funds-are-safe/src/components/pages/create/ProjectInfo.jsx
@@ -5,15 +5,21 @@ export default function ProjectInfo() {
   return (
     <div className={styles.projectInfo}>
       <div>
-          <h3>MEDIA</h3>
+        <h3>MEDIA</h3>
+        <input type="file"></input>
       </div>
       <div>
         <h3>NAME</h3>
         <input className={styles.input} placeholder="Project name" />
         <br />
         <h3>GOAL</h3>
-        <input className={styles.input} placeholder="Currency" />
-        <input className={styles.input} placeholder="Goal" />
+        <select>
+          <option value="red"> USDC</option>
+          <option value="red"> WETH</option>
+          <option value="red"> MATIC</option>
+          <option value="red"> APECOIN</option>
+        </select>
+        <input className={styles.input} placeholder="Amount" />
       </div>
     </div>
   );
