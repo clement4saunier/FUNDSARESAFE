990fe29e237dd1392449bc50702a9d1ddeccdcdb
clement <clement.saunier@epitech.eu>
2022-05-08 10:39:10 +0200

    fix ipfs http client

--- funds-are-safe/src/components/pages/Create.jsx
+++ funds-are-safe/src/components/pages/Create.jsx
@@ -1,46 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import Page from "../layout/Page";
 import { WalletContext } from "../context/Wallet";
 import { useContext } from "react";
 import FormField from "../project/FormField";
-import Select from 'react-select';
-import styles from './Create.module.css'
+import Select from "react-select";
+import styles from "./Create.module.css";
 import ProjectInfo from "./create/ProjectInfo";
+import { create } from "ipfs-http-client";
 
+const client = create("https://ipfs.infura.io:5001/api/v0");
 
 export default function Create() {
-    const { account } = useContext(WalletContext);
-    // const [select, setSelect] = useState("");
-    console.log("adresse = ", account);
-    const options = [
-        { value: 'chocolate', label: 'Chocolate' },
-        { value: 'strawberry', label: 'Strawberry' },
-        { value: 'vanilla', label: 'Vanilla' }
-    ]
-    // const handleSelectChange = (evt) => {
-    //     setSelect(evt.target.value)
-    //     console.log(select)
-    // }
-    
-    if (account) {
-        return (
-            <Page>
-                <br />
-                <h1><span style={{"color":"#23C4AA"}}>LAUNCH</span> <u>YOUR PROJECT</u></h1>
-                <br />
-                <br />
-                <ProjectInfo/>
-                <h2>TELL THE PEOPLE</h2>
-                <textarea className={styles.markdown} placeholder="The details of your project, why it'll be worht it (full markdown)"/>
-                <button className={styles.button64}><span>Mint your project</span></button>
-            </Page>
-        )
-    } else {
-        console.log("not connected");
-        return (
-            <Page>
-                Connect you to create a project
-            </Page>
-        )
-    }
+  const { account } = useContext(WalletContext);
+  const [projectInfo, setProjectInfo] = useState({});
+  const [markdown, setMarkdown] = useState("");
+
+  async function onUpload() {
+    const { file, name, goal, token } = projectInfo;
+    console.log("projectInfo:", projectInfo);
+    console.log("markdown:", markdown);
+    const ipfsImage = await client.add(projectInfo.file);
+    const metadata = {
+      name,
+      image: ipfsImage.path,
+      goal,
+      token
+    };
+    console.log("metadata", metadata);
+  }
+
+  if (account) {
+    return (
+      <Page>
+        <br />
+        <h1>
+          <span style={{ color: "#23C4AA" }}>#1 DESCRIBE</span> <u>YOUR PROJECT</u>
+        </h1>
+        <br />
+        <h2>TELL THE PEOPLE</h2>
+        <ProjectInfo
+          onStateChange={(v) => {
+            setProjectInfo(v);
+          }}
+        />
+        <h2>TELL THE PEOPLE</h2>
+        <textarea
+          className={styles.markdown}
+          onChange={(e) => setMarkdown(e.target.value)}
+          placeholder="The details of your project, why it'll be worht it (full markdown)"
+        />
+        <button onClick={onUpload} className={styles.button64}>
+          <span>Mint your project</span>
+        </button>
+      </Page>
+    );
+  } else {
+    console.log("not connected");
+    return <Page>Connect you to create a project</Page>;
+  }
 }
--- funds-are-safe/src/components/pages/create/ProjectInfo.jsx
+++ funds-are-safe/src/components/pages/create/ProjectInfo.jsx
@@ -1,14 +1,23 @@
-import { create } from "ipfs-http-client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./ProjectInfo.module.css";
 
-export default function ProjectInfo() {
+export default function ProjectInfo({ onStateChange }) {
+  const [file, setFile] = useState();
+  const [name, setName] = useState();
+  const [goal, setGoal] = useState();
+  const [token, setToken] = useState();
   const [imageUrl, setImageUrl] = useState();
 
   function onImageUpload(e) {
     const file = e.target.files[0];
+    setFile(file);
     setImageUrl(URL.createObjectURL(file));
   }
+
+  useEffect(() => {
+    onStateChange({file, name, goal, token});
+  }, [file, name, goal, token]);
+
   return (
     <div className={styles.projectInfo}>
       <div>
@@ -18,16 +27,32 @@ export default function ProjectInfo() {
       </div>
       <div>
         <h3>NAME</h3>
-        <input className={styles.input} placeholder="Name" />
+        <input
+          className={styles.input}
+          placeholder="Name"
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        />
         <br />
         <h3>GOAL</h3>
-        <select>
-          <option value="red"> USDC</option>
-          <option value="red"> WETH</option>
-          <option value="red"> MATIC</option>
-          <option value="red"> APECOIN</option>
+        <select
+          onChange={(e) => {
+            setToken(e.target.value);
+          }}
+        >
+          <option value="USDC"> USDC</option>
+          <option value="WETH"> WETH</option>
+          <option value="MATIC"> MATIC</option>
+          <option value="APECOIN"> APECOIN</option>
         </select>
-        <input className={styles.input} placeholder="Amount" />
+        <input
+          className={styles.input}
+          placeholder="Amount"
+          onChange={(e) => {
+            setGoal(e.target.value);
+          }}
+        />
       </div>
     </div>
   );
