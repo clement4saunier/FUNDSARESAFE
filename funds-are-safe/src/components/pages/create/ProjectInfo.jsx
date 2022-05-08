314514f48e62aabe9afd89144a2c3e699a217a91
clement <clement.saunier@epitech.eu>
2022-05-08 10:14:23 +0200

    image preview

--- funds-are-safe/src/components/pages/create/ProjectInfo.jsx
+++ funds-are-safe/src/components/pages/create/ProjectInfo.jsx
@@ -1,18 +1,26 @@
-import React from "react";
+import { create } from "ipfs-http-client";
+import React, { useState } from "react";
 import styles from "./ProjectInfo.module.css";
 
 export default function ProjectInfo() {
+  const [imageUrl, setImageUrl] = useState();
+
+  function onImageUpload(e) {
+    const file = e.target.files[0];
+    setImageUrl(URL.createObjectURL(file));
+  }
   return (
     <div className={styles.projectInfo}>
       <div>
         <h3>MEDIA</h3>
-        <input type="file"></input>
+        <input type="file" onChange={onImageUpload}></input>
+        <img alt="" src={imageUrl} />
       </div>
       <div>
         <h3>NAME</h3>
-        <input className={styles.input} placeholder="Project name" />
+        <input className={styles.input} placeholder="Name" />
         <br />
-        <h3 style={{"marginTop": "40%"}}>GOAL</h3>
+        <h3>GOAL</h3>
         <select>
           <option value="red"> USDC</option>
           <option value="red"> WETH</option>
